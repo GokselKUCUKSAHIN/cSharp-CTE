c145b529732c290da7a6d01b686662e8ead6a8fc
Göksel Küçükşahin <33639948+GokselKUCUKSAHIN@users.noreply.github.com>
2019-03-14 03:30:25 +0300

    Add files via upload

---
+++ DNA/DNA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "....... 0 .......";
+            string s2 = "...... o-0 ......";
+            string s3 = "..... o---0 .....";
+            string s4 = ".... o-----0 ....";
+            string s5 = ".... 0-----0 ....";
+            string s6 = ".... 0-----o ....";
+            string s7 = "..... 0---o .....";
+            string s8 = "...... 0-o ......";
+            List<string> Anim = new List<string>() { s1,s2,s3,s4,s5,s6,s7,s8};
+            int i = 0;
+            while (true != false)
+            {
+                if (i > 7)
+                    i = 0;
+                Console.WriteLine(Anim[i]);
+                i++;
+                System.Threading.Thread.Sleep(96);
+            }
+        }
+    }
+}
