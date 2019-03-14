9a26760c2f26ec50167ef9c6ed9abaa7f54c29ea
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-14 03:53:31 +0300

    Add files via upload

---
+++ Rainbow 7/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(251, 296);
+            this.button1.Margin = new System.Windows.Forms.Padding(100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(251, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 20;
+            this.trackBar1.Location = new System.Drawing.Point(12, 296);
+            this.trackBar1.Maximum = 250;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(190, 56);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 50;
+            this.trackBar1.TickFrequency = 25;
+            this.trackBar1.Value = 150;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 39);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "0";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Rainbow7";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rainbow 7/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        int value=50;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = value;  //RED
+            timer2.Interval = value;  //GREEN
+            timer3.Interval = value;  //BLUE
+            timer4.Interval = value;  //REFRESH
+        }
+        int red = 255;
+        int green = 255;
+        int blue = 0;
+        int flag = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //RED
+            if (flag == 2)
+            {
+                red -= 5;
+                if (red <= 0)
+                {
+                    red = 0;
+                    flag = 3;
+                }
+            }
+            else if (flag == 5)
+            {
+                red += 5;
+                if (red >= 255)
+                {
+                    red = 255;
+                    flag = 0;
+                }
+            }
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //GREEN
+            if(flag==0)
+            {
+                green -= 5;
+                if(green<=0)
+                {
+                    green = 0;
+                    flag = 1;
+                }
+
+            }
+            else if(flag==3)
+            {
+                green += 5;
+                if(green>=255)
+                {
+                    green = 255;
+                    flag = 4;
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            //BLUE
+            if(flag==1)
+            {
+                blue += 5;
+                if(blue>=255)
+                {
+                    blue = 255;
+                    flag = 2;
+                }
+            }
+            else if(flag==4)
+            {
+                blue -= 5;
+                if(blue<=0)
+                {
+                    blue = 0;
+                    flag = 5;
+                }
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            //Refresh
+            this.BackColor= Color.FromArgb(255, red, green, blue);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = true;
+            button1.Visible = false;
+            button2.Visible = true;
+            //trackBar1.Visible = true;
+        }
+        private void button2_Click(object sender, EventArgs e) //STOP
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+            button1.Visible = true;
+            button2.Visible = false;
+            //trackBar1.Visible = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //label1.Text = trackBar1.Value.ToString();
+            value = trackBar1.Value;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            value = trackBar1.Value;
+            label1.Text = value.ToString();
+        }
+    }
+}
+
+
+/*
+private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (flag == 0)
+            {
+                int sayac = sayi-=5;
+                this.BackColor = Color.FromArgb(255, sayi, sayi,sayi);
+                if (sayi<=0)
+                {
+                    flag = 1;
+                }
+            }
+            else
+            {
+                int sayac = sayi+=5;
+                //label1.Text = sayac.ToString();
+                this.BackColor = Color.FromArgb(sayi, sayi, sayi);
+                if (sayi >= 255)
+                {
+                    flag = 0;
+                }
+            }
+        }
+*/
