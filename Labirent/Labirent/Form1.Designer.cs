8222e7fa1e602806726e564a8edb80f50c43efef
Göksel Küçükşahin <33639948+GokselKUCUKSAHIN@users.noreply.github.com>
2019-03-14 03:35:04 +0300

    Add files via upload

---
+++ Labirent/Labirent/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Labirent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 620);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MazeGenerator - Jellybeanci";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Labirent/Labirent/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labirent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp;
+        Graphics g;
+        Random r = new Random();
+        static Pen kalem = new Pen(Color.Snow, 2f);
+        Point a = new Point(0,0);
+        Point b = new Point(10,10);
+        int i;
+        int j;
+        bool CM = false;
+        Color c = Color.Snow;
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(601, 601);
+            i = 0;
+            j = 0;
+            timer1.Start();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bmp);
+            //
+            if (j < 600)
+            {
+                if(i < 600)
+                {
+                    //draw something.
+                    if (r.Next(0, 2) % 2 == 0)
+                    {
+                        //çift
+                        a = new Point(i, j + 10);
+                        b = new Point(i + 10, j);
+                    }
+                    else
+                    {
+                        //tek
+                        a = new Point(i, j);
+                        b = new Point(i + 10, j + 10);
+                    }
+                    //
+                    if(CM)
+                    {
+                        Colorness();
+                    }
+                    g.DrawLine(kalem, a, b);
+                    //
+                    pictureBox1.Image = bmp;
+                    g.Dispose();
+                    //
+                    i += 10;
+                }
+                else
+                {
+                    i = 0;
+                    j += 10;
+                }
+            }
+            else
+            {
+                //done
+                timer1.Stop();
+                //pictureBox1.BackColor = Color.Red;
+            }
+        }
+        private void Colorness()
+        {
+            byte red = (byte)r.Next(256);
+            byte green = (byte)r.Next(256);
+            byte blue = (byte)r.Next(256);
+            c = Color.FromArgb(255, red, green, blue);
+            kalem.Color = c;
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(CM)
+            {
+                c = Color.Snow;
+                kalem.Color = c;
+                CM = false;
+            }
+            else
+            {
+                CM = true;
+            }
+        }
+    }
+}
