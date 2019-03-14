64b3ba126a7199a3db7c0dafb30aeb7e19d041a2
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-14 03:47:01 +0300

    Add files via upload

---
+++ Sine Wave/Sine Wave/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sine_Wave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                for(float t = 0; t <= 40f; t+= 0.25f)
+                {
+                    int a = (int)(27 + 25 * Math.Sin(t));
+                    for (int i = 0; i < a; i++)
+                        Console.Write(" ");
+                    Console.Write("JELLYBEANCI\n");
+                    System.Threading.Thread.Sleep(100);
+                }
+                
+            }
+        }
+    }
+}
