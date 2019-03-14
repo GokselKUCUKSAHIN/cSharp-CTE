2e5b7a8fb974459148db49b7fd156df3a3a432b4
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-14 03:48:08 +0300

    Add files via upload

---
+++ Discrete Math Homework/Rand/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int x = r.Next(0, 101);
+                Console.WriteLine(x);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
