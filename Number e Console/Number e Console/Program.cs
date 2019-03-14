02e182538fabe6c513029e58d8792ece38b44980
Göksel Küçükşahin <33639948+GokselKUCUKSAHIN@users.noreply.github.com>
2019-03-14 03:29:38 +0300

    Add files via upload

---
+++ Number e Console/Number e Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_e_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number Limit ->");
+            int limit = Int32.Parse(Console.ReadLine());
+            double last;
+            int max = (int)Math.Pow(2, 32) - 1;
+            Console.WriteLine(max);
+            last = Euler(max);
+            Console.WriteLine(last);
+
+            for (int i = 1; i <= max; i+=250)
+            {
+                Console.WriteLine("{0}-) {1}",i,Euler(i));
+                
+            }
+        }
+        private static double Euler(double n)
+        {
+            return Math.Pow(1 + 1 / n, n);
+        }
+    }
+}
