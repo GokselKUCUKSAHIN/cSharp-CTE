151e9c2ac2ac4c761fd7541eeafd3e329c8a1f50
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-24 01:30:29 +0300

    Add files via upload

---
+++ Leibniz Series Number Pi/Leibniz Series Number Pi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leibniz_Series_Number_Pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 1;
+            for (long i = 0; i < 100000000L; i++)
+            {
+                double x = i * 2 + 3;
+                if (i % 2 == 0)
+                {
+                    pi -= (1 / (double)x);
+                }
+                else
+                {
+                    pi += (1 / (double)x);
+                }
+                Console.WriteLine(4*pi);
+            }
+        }
+    }
+}
