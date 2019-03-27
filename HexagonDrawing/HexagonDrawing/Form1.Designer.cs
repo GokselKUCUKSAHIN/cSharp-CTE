31f147f5f4b9a2e6b94ee67cd4a8f34d67409932
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-27 14:31:34 +0300

    Add files via upload

---
+++ HexagonDrawing/HexagonDrawing/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace HexagonDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerR = new System.Windows.Forms.Timer(this.components);
+            this.timerG = new System.Windows.Forms.Timer(this.components);
+            this.timerB = new System.Windows.Forms.Timer(this.components);
+            this.timerI = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 618);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(578, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "XD";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(578, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "XD";
+            this.label2.Visible = false;
+            // 
+            // timerR
+            // 
+            this.timerR.Tick += new System.EventHandler(this.R_Tick);
+            // 
+            // timerG
+            // 
+            this.timerG.Tick += new System.EventHandler(this.G_Tick);
+            // 
+            // timerB
+            // 
+            this.timerB.Tick += new System.EventHandler(this.B_Tick);
+            // 
+            // timerI
+            // 
+            this.timerI.Tick += new System.EventHandler(this.I_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(624, 649);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Rainbow8.1 - Jellybeanci";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerR;
+        private System.Windows.Forms.Timer timerG;
+        private System.Windows.Forms.Timer timerB;
+        private System.Windows.Forms.Timer timerI;
+    }
+}
+
---
+++ HexagonDrawing/HexagonDrawing/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HexagonDrawing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timerR.Interval = value;  //RED
+            timerG.Interval = value;  //GREEN
+            timerB.Interval = value;  //BLUE
+            timerI.Interval = value;  //REFRESH
+        }
+        Graphics g;
+        Bitmap bmp;
+        Pen pen = new Pen(Color.FromArgb(255, 5, 5, 5), 2.6f);
+        Pen pen2 = new Pen(Color.FromArgb(230, 255, 15, 15), 4f);
+        //
+        Pen pSec = new Pen(Color.FromArgb(5, 45, 190, 150), 2.6f);
+        Pen pMin = new Pen(Color.FromArgb(200, 0, 20, 240), 2f);
+        Pen pHrs = new Pen(Color.FromArgb(200, 0, 255, 0), 2f);
+        //
+        int W;
+        int H;
+        Point C;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            W = pictureBox1.Width;
+            H = pictureBox1.Height;
+            bmp = new Bitmap(W + 1, H + 1);
+            C = new Point(W / 2, H / 2);
+            /*
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            DrawHexagon(new Point(W / 2, H / 2), 250, 0);
+            pictureBox1.Image = bmp;
+            g.Dispose();
+            */
+            timer1.Start();
+            timerR.Enabled = true;
+            timerG.Enabled = true;
+            timerB.Enabled = true;
+            timerI.Enabled = true;
+        }
+        private void DrawPolygon(Point center, int Length, int angle, int edge)
+        {
+            //calculate
+            Point[] Hexagon = new Point[edge];
+            int a = angle;
+            int offset = (int)(360 / (float)(edge));
+            for (int i = 0; i < edge; i++)
+            {
+                Hexagon[i] = EndPoint(center, a, Length);
+                a += offset;
+            }
+            //draw
+            g.DrawPolygon(pSec, Hexagon);
+        }
+        int alfa = 0;
+        int size = 200;
+        bool site = false;
+        //
+        Random rand = new Random();
+        int shape = 3;
+        bool decs = false;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bmp);
+            //g.Clear(Color.White);
+            //pSec.Color = Color.FromArgb(150,rand.Next(50,150), rand.Next(110,220), rand.Next(50,200));
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            DrawPolygon(new Point(W / 2, H / 2), size, alfa, shape);
+            pictureBox1.Image = bmp;
+            g.Dispose();
+            alfa += 1;
+            if (size >= 300)
+            {
+                site = true;
+                if (!decs)
+                {
+                    shape++;
+                }
+                else
+                {
+                    shape--;
+                }
+            }
+            else if (size <= 50)
+            {
+                site = false;
+                if (!decs)
+                {
+                    shape++;
+                }
+                else
+                {
+                    shape--;
+                }
+            }
+            if (site)
+            {
+                size -= 1;
+            }
+            else
+            {
+                size += 1;
+            }
+            if (shape > 7)
+            {
+                decs = true;
+            }
+            else if (shape < 3)
+            {
+                shape = 4;
+                decs = false;
+            }
+        }
+        //
+        //
+        private static Point EndPoint(Point o, int angle, int length)
+        {
+            var endPoint = o;
+            endPoint.X += (int)(length * Math.Cos(Radian(angle)));
+            endPoint.Y -= (int)(length * Math.Sin(Radian(angle)));
+            return endPoint;
+        }
+        private static Point FindMiddle(Point a, Point b)
+        {
+            int dx = b.X - a.X;
+            int dy = b.Y - a.Y;
+            return new Point(a.X + dx / 2, a.Y + dy / 2);
+        }
+        private static Point FirstPart(Point a, Point b)
+        {
+            int dx = b.X - a.X;
+            int dy = b.Y - a.Y;
+            return new Point(a.X + dx / 3, a.Y + dy / 3);
+        }
+        private static Point SecondPart(Point a, Point b)
+        {
+            int dx = b.X - a.X;
+            int dy = b.Y - a.Y;
+            return new Point(a.X + 2 * (dx / 3), a.Y + 2 * (dy / 3));
+        }
+        private static double Radian(double angle)
+        {
+            return (Math.PI / 180.0) * angle;
+        }
+        private static double Angle(double Radian)
+        {
+            return Radian * (180 / Math.PI);
+        }
+        private static double CalculateAngle(Point p1, Point p2)
+        {
+            double dx = Math.Abs(p1.X - p2.X);
+            double dy = Math.Abs(p1.Y - p2.Y);
+            double a = Math.Round(Angle(Math.Atan(dy / dx)));
+            if (p1.X - p2.X >= 0)
+            {
+                //II - III
+                if (p1.Y - p2.Y >= 0)
+                {
+                    //II Region
+                    return 180 - a;
+                }
+                else
+                {
+                    //III Region
+                    return 180 + a;
+                }
+            }
+            else
+            {
+                // I - IV
+                if (p1.Y - p2.Y >= 0)
+                {
+                    //I Region
+                    return a;
+                }
+                else
+                {
+                    //IV Region
+                    return 360 - a;
+                }
+            }
+        }
+        private static int CalculatLength(Point p1, Point p2)
+        {
+            double dx = Math.Abs(p1.X - p2.X);
+            double dy = Math.Abs(p1.Y - p2.Y);
+            return (int)Math.Round(Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2)));
+        }
+        //
+        int value = 100;
+        int red = 255;
+        int green = 255;
+        int blue = 0;
+        int flag = 0;
+
+        private void R_Tick(object sender, EventArgs e)
+        {
+            //RED
+            if (flag == 2)
+            {
+                red -= 5;
+                if (red <= 0)
+                {
+                    red = 0;
+                    flag = 3;
+                }
+            }
+            else if (flag == 5)
+            {
+                red += 5;
+                if (red >= 255)
+                {
+                    red = 255;
+                    flag = 0;
+                }
+            }
+
+        }
+
+        private void G_Tick(object sender, EventArgs e)
+        {
+            //GREEN
+            if (flag == 0)
+            {
+                green -= 5;
+                if (green <= 0)
+                {
+                    green = 0;
+                    flag = 1;
+                }
+
+            }
+            else if (flag == 3)
+            {
+                green += 5;
+                if (green >= 255)
+                {
+                    green = 255;
+                    flag = 4;
+                }
+            }
+        }
+
+        private void B_Tick(object sender, EventArgs e)
+        {
+            //BLUE
+            if (flag == 1)
+            {
+                blue += 5;
+                if (blue >= 255)
+                {
+                    blue = 255;
+                    flag = 2;
+                }
+            }
+            else if (flag == 4)
+            {
+                blue -= 5;
+                if (blue <= 0)
+                {
+                    blue = 0;
+                    flag = 5;
+                }
+            }
+        }
+
+        private void I_Tick(object sender, EventArgs e)
+        {
+            //Refresh
+            this.BackColor = Color.FromArgb(255, red, green, blue);
+            pSec.Color = Color.FromArgb(80, red, green, blue);
+        }
+
+    }
+}
